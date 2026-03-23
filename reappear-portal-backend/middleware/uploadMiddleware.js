efc9d2e9dd36c741bf055592e55f0b53dc92da20
itsmenandini21 <nandinimehrotra99@gmail.com>
2026-03-23 13:32:03 +0530

    c

--- reappear-portal-backend/middleware/uploadMiddleware.js
+++ reappear-portal-backend/middleware/uploadMiddleware.js
@@ -1,9 +1,21 @@
 import multer from 'multer';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uploadFolder = path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadFolder)) {
+    fs.mkdirSync(uploadFolder, { recursive: true });
+}
+
 // Configure where and how to save the file
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/'); // Saves it to the folder we just created!
+        cb(null, uploadFolder); // Saves it securely using absolute paths
     },
     filename: function (req, file, cb) {
         // Adds a timestamp so every filename is 100% unique
